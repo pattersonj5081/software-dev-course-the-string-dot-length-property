078e59a286030682b80a44e0ebc460882d15531e
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-03-18 09:11:55 -0500

    found the length of all variables

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -16,15 +16,24 @@ Your task is to:
 // Starter Code (Pre-filled):
 // Predefined strings
 let stringOne = "Coding Bootcamp";
+
+
 let stringTwo = "JavaScript";
+
+
 let stringThree = "";
 
+
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
+ 
 
 // Your code here
 
+let lengthOne = stringOne .length;
 
+let lengthTwo = stringTwo .length;
+
+let lengthThree = stringThree .length;
+
+console.log (lengthOne, lengthTwo, lengthThree);
 
